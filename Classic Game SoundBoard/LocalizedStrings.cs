96a50557af43f566b7878f8775fb413398253bd9
Redbeard2794 <jayo2794@gmail.com>
2014-11-17 22:22:15 +0000

    original commit

---
+++ Classic Game SoundBoard/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Classic_Game_SoundBoard.Resources;
+
+namespace Classic_Game_SoundBoard
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
